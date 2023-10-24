d5ccddddfd2a1e9673b09bdba66d0fc98a7cc819
chunlei.zcl <chunlei.zcl@alibaba-inc.com>
2023-10-24 13:59:02 +0800

    解决部分环境的跨域问题

--- Server/Java/code/src/main/java/com/aliyuncs/aui/filter/CorsFilter.java
+++ Server/Java/code/src/main/java/com/aliyuncs/aui/filter/CorsFilter.java
@@ -28,6 +28,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
+        response.setHeader("Access-Control-Allow-Headers", "authorization,content-type");
         response.setHeader("Access-Control-Allow-Origin", httpCorsHost);
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
